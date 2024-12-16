12d7f29b18f7574112ea8811e8c8bc5c7178a50d
Sébastien Lau <sebastien.lau2021@gmail.com>
2024-12-16 19:10:36 +0100

    change api uri

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 // const API_URL = "http://localhost:4000";
-const API_URL = 'https://oc-7-mon-vieux-grimoire-nsj80kcfv-seblau02s-projects.vercel.app';
+const API_URL = 'https://oc-7-mon-vieux-grimoire-phi.vercel.app';
+
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
